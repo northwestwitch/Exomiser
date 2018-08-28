5f5cf7be6cc8d40218a4f1cbdecb546558472835
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2018-08-28 17:13:30 +0100

    Add assembly-specific variant caches Use AlleleProto.AlleleKey as variant cache key

--- exomiser-spring-boot-autoconfigure/src/main/java/org/monarchinitiative/exomiser/autoconfigure/genome/VariantKeyGenerator.java
+++ exomiser-spring-boot-autoconfigure/src/main/java/org/monarchinitiative/exomiser/autoconfigure/genome/VariantKeyGenerator.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2017 Queen Mary University of London.
+ * Copyright (c) 2016-2018 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -20,6 +20,7 @@
 
 package org.monarchinitiative.exomiser.autoconfigure.genome;
 
+import org.monarchinitiative.exomiser.core.genome.dao.serialisers.MvStoreUtil;
 import org.monarchinitiative.exomiser.core.model.Variant;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.cache.interceptor.SimpleKey;
@@ -46,8 +47,8 @@ public class VariantKeyGenerator implements KeyGenerator {
         }
         if (params.length == 1) {
             Object param = params[0];
-            if (param != null && Variant.class.isInstance(param)) {
-                return generateVariantKey((Variant) param);
+            if (param instanceof Variant) {
+                return MvStoreUtil.generateAlleleKey((Variant) param);
             }
         }
         return new SimpleKey(params);
